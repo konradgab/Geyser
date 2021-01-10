96c513e2be863c6c31ed00421c6e0118511cf268
Jedrzej <42713788+Extollite@users.noreply.github.com>
2021-01-10 19:30:09 +0100

    Performance testing with real connection

--- test/src/test/java/org/geysermc/PerformanceTest.java
+++ test/src/test/java/org/geysermc/PerformanceTest.java
@@ -27,15 +27,25 @@ package org.geysermc;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nukkitx.protocol.bedrock.Bedrock;
 import com.nukkitx.protocol.bedrock.BedrockClient;
 import com.nukkitx.protocol.bedrock.BedrockPacket;
 import com.nukkitx.protocol.bedrock.BedrockServer;
-import com.nukkitx.protocol.bedrock.packet.TextPacket;
+import lombok.Getter;
+import org.geysermc.connector.GeyserConnector;
 import org.geysermc.connector.network.BedrockProtocol;
+import org.geysermc.connector.network.translators.PacketTranslatorRegistry;
+import org.geysermc.platform.standalone.GeyserStandaloneBootstrap;
 import org.geysermc.util.handler.TestServerEventHandler;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -46,7 +56,7 @@ import java.util.Map;
 import static org.geysermc.util.helper.TestHelper.startBedrockClient;
 
 public class PerformanceTest {
-    private static final int WARM_UP_ITERATIONS = 10;
+    private static final int WARM_UP_ITERATIONS = 3;
     private static final int TEST_ITERATIONS = 10;
 
     private final ObjectMapper JSON_MAPPER = new ObjectMapper().disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
@@ -56,103 +66,131 @@ public class PerformanceTest {
     private final Map<BedrockPacket, List<Long>> warmUpGeyserClientConnectionTimes = new HashMap<>();
     private final Map<BedrockPacket, List<Long>> geyserClientConnectionTimes = new HashMap<>();
 
-    private final Map<BedrockPacket, Long> clientPackets = new LinkedHashMap<>();
+    private Map<BedrockPacket, Long> clientPackets = new LinkedHashMap<>();
 
     @Before
-    public void setUp() {
+    @SuppressWarnings("unchecked")
+    public void setUp() throws InterruptedException {
+        SpigotRunnable runnable = new SpigotRunnable();
+        Thread spigotThread = new Thread(runnable, "spigot");
+        spigotThread.start();
 
+        new Thread(() -> GeyserStandaloneBootstrap.main(new String[] {"--nogui"}), "geyser").start();
+
+        while (GeyserConnector.getInstance() == null) {
+            Thread.sleep(1000);
+        }
+
+        while(GeyserConnector.getInstance().getPlayers().size() != 1) {
+            Thread.sleep(1000);
+        }
+
+        while(GeyserConnector.getInstance().getPlayers().size() == 1) {
+            Thread.sleep(1000);
+        }
+
+        GeyserConnector.getInstance().shutdown();
+
+        try {
+            runnable.getWriter().write("stop\n");
+            runnable.getWriter().flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        clientPackets = PacketTranslatorRegistry.clientPackets;
+
+        for (Map.Entry<BedrockPacket, Long> entry : clientPackets.entrySet()) {
+            warmUpDirectClientConnectionTimes.put(entry.getKey(), new ArrayList<>());
+            directClientConnectionTimes.put(entry.getKey(), new ArrayList<>());
+        }
+
+        System.out.println(clientPackets);
     }
 
     @Test
     public void directClientConnection() throws Exception {
         Map<BedrockPacket, Long> sendPacket = new HashMap<>();
 
-        TextPacket example = new TextPacket();
-        example.setXuid("0");
-        example.setSourceName("Test");
-        example.setMessage("Test");
-        example.setType(TextPacket.Type.CHAT);
+        BedrockServer server = new BedrockServer(new InetSocketAddress("0.0.0.0", 19132));
+        server.setHandler(new TestServerEventHandler(sendPacket, warmUpDirectClientConnectionTimes));
 
-        directClientConnectionTimes.put(example, new ArrayList<>());
-        warmUpDirectClientConnectionTimes.put(example, new ArrayList<>());
+        server.bind().join();
+
+        BedrockClient client = startBedrockClient();
+
+        InetSocketAddress connectionAddress = new InetSocketAddress("127.0.0.1", 19132);
+        client.connect(connectionAddress).join().setPacketCodec(BedrockProtocol.DEFAULT_BEDROCK_CODEC);
+        client.getSession().setLogging(false);
+
+        while (server.getRakNet().getSessionCount() != 1) {
+            Thread.sleep(10);
+        }
 
         // WARM UP
         for (int i = 0; i < WARM_UP_ITERATIONS; i++) {
+            System.out.println("Warmpup " + i);
 
-            BedrockServer server = new BedrockServer(new InetSocketAddress("0.0.0.0", 19132));
-            server.setHandler(new TestServerEventHandler(sendPacket, warmUpDirectClientConnectionTimes));
+            for (Map.Entry<BedrockPacket, Long> entry : clientPackets.entrySet()) {
+                sendPacket.put(entry.getKey(), System.currentTimeMillis());
+                client.getSession().sendPacket(entry.getKey());
 
-            server.bind().join();
-
-            BedrockClient client = startBedrockClient();
-
-            InetSocketAddress connectionAddress = new InetSocketAddress("127.0.0.1", 19132);
-            client.connect(connectionAddress).join().setPacketCodec(BedrockProtocol.DEFAULT_BEDROCK_CODEC);
-            client.getSession().setLogging(false);
-
-            while (server.getRakNet().getSessionCount() != 1) {
-                Thread.sleep(200);
+                Thread.sleep(entry.getValue());
             }
-
-//        for (Map.Entry<BedrockPacket, Long> entry : clientPackets.entrySet()) {
-//            sendPacket.put(entry.getKey(), System.currentTimeMillis());
-//            client.getSession().sendPacket(entry.getKey());
-//
-//            Thread.sleep(entry.getValue());
-//        }
-
-            sendPacket.put(example, System.currentTimeMillis());
-
-            client.getSession().sendPacketImmediately(example);
 
             while (!sendPacket.isEmpty()) {
-                Thread.sleep(100);
+                Thread.sleep(10);
             }
-
-            client.close();
-            server.close();
         }
 
-        sendPacket.clear();
+        server.setHandler(new TestServerEventHandler(sendPacket, directClientConnectionTimes));
+
+        client.close();
+
+        client = startBedrockClient();
+        client.connect(connectionAddress).join().setPacketCodec(BedrockProtocol.DEFAULT_BEDROCK_CODEC);
+        client.getSession().setLogging(false);
+
+        while (server.getRakNet().getSessionCount() != 1) {
+            Thread.sleep(10);
+        }
 
         for (int i = 0; i < TEST_ITERATIONS; i++) {
-            BedrockServer server = new BedrockServer(new InetSocketAddress("0.0.0.0", 19132));
-            server.setHandler(new TestServerEventHandler(sendPacket, directClientConnectionTimes));
+            System.out.println(i);
 
-            server.bind().join();
+            for (Map.Entry<BedrockPacket, Long> entry : clientPackets.entrySet()) {
+                sendPacket.put(entry.getKey(), System.currentTimeMillis());
+                client.getSession().sendPacket(entry.getKey());
 
-            BedrockClient client = startBedrockClient();
-
-            InetSocketAddress connectionAddress = new InetSocketAddress("127.0.0.1", 19132);
-            client.connect(connectionAddress).join().setPacketCodec(BedrockProtocol.DEFAULT_BEDROCK_CODEC);
-            client.getSession().setLogging(false);
-
-            while (server.getRakNet().getSessionCount() != 1) {
-                Thread.sleep(200);
+                Thread.sleep(entry.getValue());
             }
-
-//        for (Map.Entry<BedrockPacket, Long> entry : clientPackets.entrySet()) {
-//            sendPacket.put(entry.getKey(), System.currentTimeMillis());
-//            client.getSession().sendPacket(entry.getKey());
-//
-//            Thread.sleep(entry.getValue());
-//        }
-
-            sendPacket.put(example, System.currentTimeMillis());
-
-            client.getSession().sendPacketImmediately(example);
 
             while (!sendPacket.isEmpty()) {
-                Thread.sleep(100);
+                Thread.sleep(10);
             }
-
-            client.close();
-            server.close();
         }
+
+        client.close();
+        server.close();
 
         System.out.println(warmUpDirectClientConnectionTimes);
         System.out.println(directClientConnectionTimes);
     }
+}
 
+@Getter
+class SpigotRunnable implements Runnable {
+    private BufferedWriter writer;
 
+    @Override
+    public void run() {
+        try {
+            Process proc = Runtime.getRuntime().exec("java -jar paper-1.16.4.jar nogui", null, new File("/Users/extollite/Documents/GitHub/Geyser-test/test/spigot"));
+            writer =  new BufferedWriter(new OutputStreamWriter(proc.getOutputStream()));
+            new BufferedReader(new InputStreamReader(proc.getInputStream())).lines().forEach(s -> System.out.println("[SPIGOT] "+s));
+            proc.waitFor();
+        } catch (InterruptedException | IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
